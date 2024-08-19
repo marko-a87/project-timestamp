aa69d4e49f7da1ac429c577380a8295891c44133
marko-a87 <malco0253@gmail.com>
2024-08-19 15:26:39 -0500

    changed imports for code

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require("cors");
+import cors from "cors";
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
